946b795d92636292661d9a02b82d962ee13888c9
Aldrin <gamaaldrin@gmail.com>
2020-11-02 13:55:32 +0200

    Add .gitignore file

---
+++ Mars Mission/Mars MissionTests/Mars_MissionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mars_MissionTests.swift
+//  Mars MissionTests
+//
+//  Created by Aldrin Bahle Gama on 2020/11/02.
+//
+
+import XCTest
+@testable import Mars_Mission
+
+class Mars_MissionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
