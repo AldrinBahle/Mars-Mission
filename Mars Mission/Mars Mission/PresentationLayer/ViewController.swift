2e4cb432bb2abc39687564e79896bc1b5e25b887
Aldrin <gamaaldrin@gmail.com>
2020-11-03 17:08:48 +0200

    architecture refractoring

--- Mars Mission/Mars Mission/PresentationLayer/ViewController.swift
+++ Mars Mission/Mars Mission/PresentationLayer/ViewController.swift
@@ -11,20 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let data = fetchData()
-        self.displayData(data: data)
+//        let data = fetchData()
+//        self.displayData(data: data)
     }
     
-    private func fetchData() -> [String] {
-        return []
-    }
-
-    private func displayData(data: [String]) {
-        for element in data {
-            let label = UILabel()
-            label.text = element
-            stackView.addArrangedSubview(label)
-        }
-    }
+//    private func fetchData() -> [String] {
+//        return []
+//    }
+//
+//    private func displayData(data: [String]) {
+//        for element in data {
+//            let label = UILabel()
+//            label.text = element
+//            stackView.addArrangedSubview(label)
+//        }
+//    }
 }
 
